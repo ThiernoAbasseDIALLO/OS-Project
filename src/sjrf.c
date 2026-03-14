a0bc68167b5c8ddcef5853ae005c03fbafd8e1d7
Thierno Abasse DIALLO <diallo980211@gmail.com>
2026-03-14 01:22:56 +0100

    Algorithme du SJRF, avec toutes la logique

--- src/sjrf.c
+++ src/sjrf.c
@@ -1,8 +1,90 @@
 #include <stdio.h>
 #include "../include/sjrf.h"
 
-void run_sjrf(processus_t *processus, int n) {
+static processus_t *selectionner_processus(processus_t *processus, int n, int t);
 
+void run_sjrf(processus_t *processus, int n) {
+	int t = 0;
+	int termines = 0;
+	int temps_non_occupation = 0;
+
+	while (termines < n) {
+		/*
+		 * Cette boucle permet de repasser les etats des processus en éxecution en prêt à chaque tour.
+		 */
+		for (int m = 0; m < n; m++) {
+			if (processus[m].etat == ETAT_EN_EXECUTION)
+				processus[m].etat = ETAT_PRET;
+		}
+
+		/*
+		 * Cela permet à chaque t temps d'arrivée de mettre l'état des processus en prêt, qui permettra par la suite de
+		 * sélectionner le plus court temps de processus restant.
+		 */
+		for (int i = 0; i < n; i++) {
+			if (processus[i].temps_arrivee == t)
+				processus[i].etat = ETAT_PRET;
+		}
+
+		/*
+		 * Permet de décrémenter les E/S (I/O) en cours.
+		 */
+		for (int j = 0; j < n; j++) {
+			if (processus[j].etat == ETAT_EN_ATTENTE) {
+				processus[j].temps_io_restant--;
+				if (processus[j].temps_io_restant == 0) {
+					int index = ++processus[j].index_burst_courant;
+					processus[j].temps_cpu_restant = processus[j].bursts[index];
+					processus[j].etat = ETAT_PRET;
+				}
+			}
+		}
+
+		/*
+		 * Ici, on sélectionne le plus court temps de cpu restant.
+		 * Si on n'a pas de processus, donc on incrémente le temps de non-occupation du cpu.
+		 * Sinon, si c'est la première éxécution du processus, on enregistre le temps de reponse et de début d'exécution à t.
+		 * On passe l'état du processus en éxécution, et on décrémente le temps de cpu restant du processus.
+		 * Si le temps de cpu restant est à 0, on vérifie si c'est le dernier burst l'état du processus passe à terminer,
+		 * on enregistre le temps de fin d'exécution et on incrémente les processus términés.
+		 * Sinon le prochain burst c'est en E/S (I/O) et l'état, on le met en attente.
+		 */
+		processus_t *courant = selectionner_processus(processus, n, t);
+		if (courant == NULL) {
+			temps_non_occupation++;
+		}else {
+			if (courant->first_run == 0) {
+				courant->temps_reponse = t;
+				courant->temps_debut_execution = t;
+				courant->first_run = 1;
+			}
+
+			courant->etat = ETAT_EN_EXECUTION;
+			courant->temps_cpu_restant--;
+
+			if (courant->temps_cpu_restant == 0) {
+				courant->index_burst_courant++;
+				if (courant->index_burst_courant == courant->nb_bursts) {
+					courant->temps_fin_execution = t+1;
+					courant->etat = ETAT_TERMINE;
+					termines++;
+				}else {
+					courant->temps_io_restant = courant->bursts[courant->index_burst_courant];
+					courant->etat = ETAT_EN_ATTENTE;
+				}
+			}
+		}
+
+		/*
+		 * Les processus prêts qui ne sont pas élus, on augmente leurs temps d'attente.
+		 */
+		for (int l = 0; l < n; l++) {
+			if (processus[l].etat == ETAT_PRET)
+				processus[l].temps_attente++;
+		}
+
+		t += 1;
+	}
 }
 
 /**
